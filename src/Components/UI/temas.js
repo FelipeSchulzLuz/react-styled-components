736ef552f664f0ca84e7719dbd37dc352d35740e
Felipe Schulz <Schulz.neo@gmail.com>
2021-07-01 11:33:43 -0300

    criando arquivo de configuração de temas

---
+++ src/Components/UI/temas.js
@@ -0,0 +1,13 @@
+import { fundoClaro, conteudoClaro, fundoEscuro, conteudoEscuro, textoFundoEscuro, textoFundoClaro } from './variaveis'
+
+export const temaClaro = {
+    body:fundoClaro,
+    inside:conteudoClaro,
+    text: textoFundoClaro
+}
+
+export const temaEscuro = {
+    body:fundoEscuro,
+    inside:conteudoEscuro,
+    text: textoFundoEscuro
+}
